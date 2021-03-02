30c915c2911b94c75bf8b121072bf7fe4d99f16f
eligarlo <eligarlo.developer@gmail.com>
2021-03-02 19:58:08 +0200

    Single Project Template

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toogleDetails">{{ project.title }}</h3>
+    </div>
+    <div class="details" v-if="showDetails">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: Object,
+  },
+  data() {
+    return {
+      showDetails: false,
+    }
+  },
+  methods: {
+    toogleDetails() {
+      this.showDetails = !this.showDetails
+    },
+  },
+}
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  background-color: #fff;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3);
+  border-left: 4px solid #e90074;
+}
+
+h3 {
+  cursor: pointer;
+}
+</style>
